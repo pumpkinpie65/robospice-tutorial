5c913ea5c17f08e09c11f55d0e61b4ec54f72f68
pumpkinpie65 <pumpkinpie65@live.com>
2014-08-03 11:29:04 -0700

    made BaseActivity abstract and removed onCreate method

--- src/com/sliceofandroid/robospicetutorial/BaseActivity.java
+++ src/com/sliceofandroid/robospicetutorial/BaseActivity.java
@@ -1,16 +1,9 @@
 package com.sliceofandroid.robospicetutorial;
 
-import com.sliceofandroid.robospicetutorial.R;
 import android.app.Activity;
-import android.os.Bundle;
 
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
-
-    }
+	
 
 }
