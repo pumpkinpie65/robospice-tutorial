d1d4d2aad43eb58f502375237e80032f8e76d972
pumpkinpie65 <pumpkinpie65@live.com>
2014-08-03 10:57:21 -0700

    removed unneccessary menu

--- src/com/sliceofandroid/robospicetutorial/BaseActivity.java
+++ src/com/sliceofandroid/robospicetutorial/BaseActivity.java
@@ -1,9 +1,8 @@
 package com.sliceofandroid.robospicetutorial;
 
+import com.sliceofandroid.robospicetutorial.R;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class BaseActivity extends Activity {
 
@@ -12,26 +11,6 @@ public class BaseActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.base, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
