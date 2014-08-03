1f5a1b7d841c06c704f4c0a5c5188eb87d8bd049
pumpkinpie65 <pumpkinpie65@live.com>
2014-08-03 11:36:38 -0700

    added SpiceManager to BaseActivity

--- src/com/sliceofandroid/robospicetutorial/BaseActivity.java
+++ src/com/sliceofandroid/robospicetutorial/BaseActivity.java
@@ -2,8 +2,23 @@ package com.sliceofandroid.robospicetutorial;
 
 import android.app.Activity;
 
+import com.octo.android.robospice.Jackson2SpringAndroidSpiceService;
+import com.octo.android.robospice.SpiceManager;
+
 public abstract class BaseActivity extends Activity {
 
-	
+	protected SpiceManager spiceManager = new SpiceManager(Jackson2SpringAndroidSpiceService.class);
+
+	@Override
+	protected void onStart() {
+	  super.onStart();
+	  spiceManager.start(this);
+	}
+
+	@Override
+	protected void onStop() {
+	  spiceManager.shouldStop();
+	  super.onStop();
+	}
 
 }
